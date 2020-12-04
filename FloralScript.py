acaf142ca57eef9791bca8ff1670b196f67c0244
FloralHacker <75477941+FloralHacker@users.noreply.github.com>
2020-12-04 21:41:19 +1100

    Add files via upload

---
+++ FloralScript.py
@@ -0,0 +1,12 @@
+print('Welcome to the recruitment process of this internship programme. Please answer each question with capital letters.')
+Name = input('What is your full name? ').split()
+ID = input('What is your student ID? ')
+if Name == "FloralHacker":
+      print('Please press enter to continue.')
+elif ID == "396":
+      print('Congratulations! You have been accepted into the programme. Welcome aboard!')
+else:
+      print('Unfortunately, you do not meet the criteria for the programme. Have a nice day.')
+
+
+# I would like to personally thank the instructor reviewing this video for taking the time out of their day to look at and review this.
