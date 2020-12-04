57f48c97e6f35c3ced86cb14e7e6b9e66566505f
FloralHacker <75477941+FloralHacker@users.noreply.github.com>
2020-12-04 22:13:37 +1100

    Update FloralScript.py

--- FloralScript.py
+++ FloralScript.py
@@ -1,11 +1,8 @@
 # This is a basic script that interviews you for an internship programme based on a confirmation of your name and Student ID.
 
 print('Welcome to the interview process of this programme. Please answer each question with capital letters where appropriate.')
-
-# Defining Name and Student ID variables
 Name = input('What is your full name? ').split()
 ID = input('What is your Student ID? ')
-
 if Name == "FloralHacker":
       print('Please press enter to continue.')
 elif ID == "396":
